9be1ebfd21487dd57d42eeb724088799d8bf5dfa
Richard Almonte <richard.almonte@bridgesite.io>
2018-10-25 16:15:14 -0400

    add local db

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/passport').then(() => {
+mongoose.connect('mongodb://localhost/trendsters').then(() => {
   console.log(`🤖  MongoDB is alive and ready to serve.`)
 })
 
