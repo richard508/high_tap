6ffba70e030ecaddb05ee5b5a39081ac6fa81de0
Richard Almonte <richard.almonte@bridgesite.io>
2018-10-25 20:17:24 -0400

    add edit VC

--- controllers/user.js
+++ controllers/user.js
@@ -12,11 +12,24 @@ const userController = {
           })
         })
       })
-    })
+    } else {
+      res.redirect(`/`)
+    }
   },
-  edit: (req, res) => {},
-  update: (req, res) => {},
-  delete: (req, res) => {}
+  edit: (req, res) => {
+    if (req.isAuthenticated()) { 
+      Interest.find().then((interest) => {
+        User.findById(req.params.id).populate('interests').then(userFromDb => {
+          res.render('user/edit', { 
+            user: userFromDb,
+            interest: interest
+          })
+        })
+      })
+    } else {
+      res.redirect(`/`)
+    }
+  },
 }
 
 module.exports = userController
