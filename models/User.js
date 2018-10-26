8c096ddb94fb479a3f9753673ae585b8216911d7
Richard Almonte <richard.almonte@bridgesite.io>
2018-10-26 11:47:43 -0400

    add secondary demograpgic to user

--- models/User.js
+++ models/User.js
@@ -7,6 +7,9 @@ const User = mongoose.Schema({
   username: String,
   email: String,
   password: String,
+  age: Number,
+  familyStatus: String,
+  location: String,
   active: {
     type: Boolean,
     default: false
