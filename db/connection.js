9afa389ea5fe31e460c77c761fc5583cada0aa22
Richard Almonte <richard.almonte@bridgesite.io>
2018-10-30 15:50:34 -0400

    Updated DB connection code to use MongoLab when in Production

--- db/connection.js
+++ db/connection.js
@@ -2,12 +2,20 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 
+// Connect to database
 if (process.env.MONGODB_URI) {
-    mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
-} else {
-  mongoose.connect('mongodb://localhost/trendsters', { useNewUrlParser: true }).then(() => {
-    console.log(`🤖  MongoDB is alive and ready to serve.`)
+    mongoose.connect(process.env.MONGODB_URI);
+  }
+  else {
+    mongoose.connect('mongodb://localhost/express-movies')
+  }
+  mongoose.connection.on('error', function(err) {
+    console.error('MongoDB connection error: ' + err);
+    process.exit(-1);
+    }
+  )
+  mongoose.connection.once('open', function() {
+    console.log("Mongoose has connected to MongoDB!")
   })
-}
-
+  
 module.exports = mongoose
